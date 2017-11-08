68ff3e410cb07e69267bd397dbe3d96afba5a1ec
JonDuncanson-abertay <32883414+JonDuncanson-abertay@users.noreply.github.com>
2017-11-08 15:44:22 +0000

    Player Input
    Player Input half done

--- Character.py
+++ Character.py
@@ -6,5 +6,28 @@ class Character():
         self.y = y
         self.surface = Surface((40, 40))
         self.surface.fill(Color(255, 0, 0))
+        self.yvelocity = 10
+    def update(self, dt, input_):
+        if (input_.getKeys()[K_UP]):
+            self.y -= (1 * dt)
+        elif (input_.getKeys()[K_DOWN]):
+            self.y += (1 * dt)
+
+        if (self.y < 200):
+            self.y = 200
+
+        if (self.y > 400):
+            self.y = 400
+            
+        if (self.y < 300 and not input_.getKeys()[K_UP]):
+            self.yvelocity = 5 * dt
+            self.y += self.yvelocity
+
+        if (self.y > 300 and not input_.getKeys()[K_DOWN]):
+            self.yvelocity = 5 * dt
+            self.y -= self.yvelocity
+
+        if (self.y == 300):
+            self.yvelocity = 0
     def render(self, screen):
         screen.blit(self.surface, (self.x, self.y))
--- game.py
+++ game.py
@@ -7,15 +7,20 @@ from Note import Note
 if __name__ == "__main__":
     pygame.init()
     pygame.mixer.init()
-    character = Note(400, 300, "n")
+    clock = pygame.time.Clock()
+    character = Character(400, 300)
     input_ = Input()
     screen = pygame.display.set_mode((800, 600))
     pygame.display.set_caption("whitebear")
+    clock.tick()
     while (True):
+        screen.fill(pygame.Color(0, 0, 0))
         for event in pygame.event.get():
             if (event.type == pygame.QUIT):
                 pygame.display.quit()
                 sys.exit()
         input_.update()
+        character.update(clock.get_time(), input_)
         character.render(screen)
         pygame.display.update()
+        clock.tick()
