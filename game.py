b9c6af0f98f0dfea02dd839ff9ad556144bc50de
JonDuncanson-abertay <32883414+JonDuncanson-abertay@users.noreply.github.com>
2017-11-08 17:46:11 +0000

    Final Commit!
    Fixed the escape key quit function!

--- game.py
+++ game.py
@@ -23,7 +23,7 @@ if __name__ == "__main__":
                 pygame.display.quit()
                 sys.exit()
         input_.update()
-        if (input_.getKeys()[K_ESC]):
+        if (input_.getKeys()[K_ESCAPE]):
             pygame.display.quit()
             sys.exit()
         character.update(clock.get_time(), input_)
